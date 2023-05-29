2ad9a7bd37c50e807357ab3664ea56eb8f4ecfa9
Vinit Gore <72271049+Vinit-source@users.noreply.github.com>
2023-05-29 10:10:06 +0530

    Added example

---
+++ src/day05/Student.java
@@ -0,0 +1,17 @@
+package day05;
+
+public class Student {
+    String name; // Field 1
+    int age; // Field 2
+
+    void sayHello() { // Method
+        System.out.println("Hello, my name is " + name + ", and I am " + age + " years old.");
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student(); // Creating a new Student object
+        student1.name = "John";
+        student1.age = 20;
+        student1.sayHello(); // Outputs: Hello, my name is John, and I am 20 years old.
+    }
+}
